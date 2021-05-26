7bb346a64f25cc5d764ab3290291eaa1fb5b3397
Lashchinskiy Artyom <lashchinskiy artyom>
2021-05-26 10:10:11 +0300

    Adding Pages

---
+++ pages/news/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Detail news page</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route)
+  },
+}
+</script>
---
+++ pages/news/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>News (all news page)</div>
+</template>
+
+<script>
+export default {}
+</script>
