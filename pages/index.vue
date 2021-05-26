89ca283bf72b1193fb6f78751afa381bb595332a
Lashchinskiy Artyom <lashchinskiy artyom>
2021-05-26 11:23:48 +0300

    Using Store

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@
       <b-col cols="4" lg="6">Hello</b-col>
       <b-col cols="8" lg="6">World</b-col>
     </b-row>
+
+    Счётчик: {{ $store.state.counter.counter }}
+    <b-btn v-on:click="$store.commit('counter/increment')">Добавить</b-btn>
+    <b-btn v-on:click="$store.commit('counter/decrement')">Убавить</b-btn>
   </b-container>
 </template>
 
